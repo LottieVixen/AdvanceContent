823e3f65069e8c4b257dfcd26642e6d6377659fb
Charlotte Lily-Llaitchee Vixen <CharlotteLilyFields48@gmail.com>
2020-12-03 18:54:46 +0000

    tided up main

--- scripts/main.js
+++ scripts/main.js
@@ -1,97 +1,95 @@
-if(typeof(require) !== "undefined"){
-	//turrets
-	require("turrets/fallout");
-	require("turrets/calamity");
-	require("turrets/calamityii");
-	require("turrets/mirage");
-	require("turrets/illusion");
-	require("turrets/ghost");
-	require("turrets/omniturret");
-	require("turrets/shotgun");
-	require("turrets/railgun");
-	require("turrets/railgunii");
-	require("turrets/point-defence");
-	require("turrets/point-defence-ii");
-	require("turrets/static");
-	require("turrets/arccaster");
-	require("turrets/arcstorm");
-	require("turrets/eclipse");
-	require("turrets/phot");
-	require("turrets/elec");
-	require("turrets/grvt");
-	require("turrets/prot");
-	require("turrets/neut");
-	require("turrets/gluon");
-	require("turrets/bosonw");
-	require("turrets/bosonz");
-	require("turrets/bosonhiggs");
-	require("turrets/singularity");
-	require("turrets/muon");
-	require("turrets/ephemeron");
-	require("turrets/excalibur");
-	require("turrets/neptune");
-	require("turrets/spectra");
-	require("turrets/rainbow");
-	require("turrets/prism");
-	require("turrets/darkside");
-	require("turrets/refract");
-	require("turrets/crusader");
-	require("turrets/xenon");
-	require("turrets/fahr");
-	require("turrets/kelv");
-	require("turrets/cels");
-	require("turrets/fuseii");
-	require("turrets/fuseiii");
-	require("turrets/shatterf");
-	require("turrets/quakef");
-	require("turrets/cycloneii");
-	require("turrets/typhn");
-	require("turrets/cube");
-	require("turrets/endgame");
-	require("turrets/kamib");
-	//walls
-	require("advancewall");
-	require("advanceb");
-	//power
-	require("battery");
-	//crafting
-	require("coolant");
-	require("acompressor");
-	require("fission");
-	require("fus");
-	require("synth");
-	require("fissionmkii");
-	//mech
-	require("blad");
-	require("claymre");
-	require("strdust");
-	require("solar");
-	require("overgrowth");
-	//unit
-	require("strider");
-	require("disintegrator");
-	require("pandora");
-	require("ravager");
-	require("angel");
-	require("empunit");
-	require("pestilence");
-	require("famine");
-	require("death");
-	require("war");
-	require("phoenix");
-	require("storm");
-	require("scourge");
-	require("azathoth");
-	//etc
-	require("modsettings");
-	require("rturret");
-	require("effectlib");
-	require("funclib");
-	require("spritebatchcustom");
-	require("itemscorelib");
-	require("blockscore");
-	
-	//require("pandora");
-	//require("turrets/omniturret");
-	//require("tempwall");
-}
+//turrets
+require("turrets/fallout");
+require("turrets/calamity");
+require("turrets/calamityii");
+require("turrets/mirage");
+require("turrets/illusion");
+require("turrets/ghost");
+require("turrets/omniturret");
+require("turrets/shotgun");
+require("turrets/railgun");
+require("turrets/railgunii");
+require("turrets/point-defence");
+require("turrets/point-defence-ii");
+require("turrets/static");
+require("turrets/arccaster");
+require("turrets/arcstorm");
+require("turrets/eclipse");
+require("turrets/phot");
+require("turrets/elec");
+require("turrets/grvt");
+require("turrets/prot");
+require("turrets/neut");
+require("turrets/gluon");
+require("turrets/bosonw");
+require("turrets/bosonz");
+require("turrets/bosonhiggs");
+require("turrets/singularity");
+require("turrets/muon");
+require("turrets/ephemeron");
+require("turrets/excalibur");
+require("turrets/neptune");
+require("turrets/spectra");
+require("turrets/rainbow");
+require("turrets/prism");
+require("turrets/darkside");
+require("turrets/refract");
+require("turrets/crusader");
+require("turrets/xenon");
+require("turrets/fahr");
+require("turrets/kelv");
+require("turrets/cels");
+require("turrets/fuseii");
+require("turrets/fuseiii");
+require("turrets/shatterf");
+require("turrets/quakef");
+require("turrets/cycloneii");
+require("turrets/typhn");
+require("turrets/cube");
+require("turrets/endgame");
+require("turrets/kamib");
+//walls
+require("advancewall");
+require("advanceb");
+//power
+require("battery");
+//crafting
+require("coolant");
+require("acompressor");
+require("fission");
+require("fus");
+require("synth");
+require("fissionmkii");
+//mech
+require("blad");
+require("claymre");
+require("strdust");
+require("solar");
+require("overgrowth");
+//unit
+require("strider");
+require("disintegrator");
+require("pandora");
+require("ravager");
+require("angel");
+require("empunit");
+require("pestilence");
+require("famine");
+require("death");
+require("war");
+require("phoenix");
+require("storm");
+require("scourge");
+require("azathoth");
+//etc
+require("modsettings");
+require("rturret");
+require("effectlib");
+require("funclib");
+require("spritebatchcustom");
+require("itemscorelib");
+require("blockscore");
+
+//require("pandora");
+//require("turrets/omniturret");
+//require("tempwall");
